af053d652c731f3ab3e8debb56f535708f3f9d4d
AmanRai-2702 <amanrai13042004@gmail.com>
2025-04-23 11:02:18 +0530

    Add files via upload

---
+++ PROJECT_PLANT/main.py
@@ -0,0 +1,74 @@
+import os
+import json
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import streamlit as st
+
+# Define the full paths for the model and class indices files
+model_path = r"C:\Users\Aman Rai\Downloads\PROJECT_PLANT\plant_disease_prediction_model.h5"
+class_indices_path = r"C:\Users\Aman Rai\Downloads\PROJECT_PLANT\class_indices.json"
+
+# Debug: Print paths to ensure they are correct
+print(f"Model Path: {model_path}")
+print(f"Class Indices Path: {class_indices_path}")
+
+# Check if the model file exists
+if not os.path.exists(model_path):
+    raise FileNotFoundError(f"Model file not found at: {model_path}")
+else:
+    print("Model file found!")
+
+# Load the pre-trained model
+model = tf.keras.models.load_model(model_path)
+
+# Check if the class indices file exists
+if not os.path.exists(class_indices_path):
+    raise FileNotFoundError(f"Class indices file not found at: {class_indices_path}")
+else:
+    print("Class indices file found!")
+
+# Load the class indices
+with open(class_indices_path, 'r') as f:
+    class_indices = json.load(f)
+
+# Function to Load and Preprocess the Image using Pillow
+def load_and_preprocess_image(image_path, target_size=(224, 224)):
+    # Load the image
+    img = Image.open(image_path)
+    # Resize the image
+    img = img.resize(target_size)
+    # Convert the image to a numpy array
+    img_array = np.array(img)
+    # Add batch dimension
+    img_array = np.expand_dims(img_array, axis=0)
+    # Scale the image values to [0, 1]
+    img_array = img_array.astype('float32') / 255.0
+    return img_array
+
+# Function to Predict the Class of an Image
+def predict_image_class(model, image_path, class_indices):
+    preprocessed_img = load_and_preprocess_image(image_path)
+    predictions = model.predict(preprocessed_img)
+    predicted_class_index = np.argmax(predictions, axis=1)[0]
+    predicted_class_name = class_indices[str(predicted_class_index)]
+    return predicted_class_name
+
+# Streamlit App
+st.title('AgroVision')
+
+uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    col1, col2 = st.columns(2)
+
+    with col1:
+        resized_img = image.resize((150, 150))
+        st.image(resized_img)
+
+    with col2:
+        if st.button('Classify'):
+            # Preprocess the uploaded image and predict the class
+            prediction = predict_image_class(model, uploaded_image, class_indices)
+            st.success(f'Prediction: {str(prediction)}')
